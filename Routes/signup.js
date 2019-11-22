a21bbaea6ca6d4ea0c1e0018ce5586a1db580429
Yogendra <yogendra18@navgurukul.org>
2019-11-22 22:13:03 +0530

    updated Google-SignUp

--- Routes/signup.js
+++ Routes/signup.js
@@ -1,4 +1,5 @@
 const config = require("../config").envdata;
+var verifier = require("google-id-token-verifier");
 
 module.exports = function(signup, jwt, Auth) {
     signup.post("/signup", (req, res) => {
@@ -17,51 +18,107 @@ module.exports = function(signup, jwt, Auth) {
 
     signup.post("/googleSignUp", (req, res) => {
         // console.log(req.body);
-        let signupData = req.body;
-        if (req.body.familyName == undefined) {
-            signupData.familyName = undefined;
-        }
-        let finalSignUpDetails = {
-            firstName: signupData.givenName,
-            lastName: signupData.familyName,
-            email: signupData.email,
-            password: undefined
-        };
+        // let signupData = req.body;
+        // if (req.body.familyName == undefined) {
+        //     signupData.familyName = undefined;
+        // }
+        // let finalSignUpDetails = {
+        //     firstName: signupData.givenName,
+        //     lastName: signupData.familyName,
+        //     email: signupData.email,
+        //     password: undefined
+        // };
 
-        Auth.findOrCreate({
-            where: {
-                firstName: signupData.given_name,
-                email: signupData.email
-            },
-            defaults: finalSignUpDetails,
-            raw: true
-        })
-            .then(([exist, create]) => {
-                console.log("manual", exist);
-                console.log("mau", create);
+        // Auth.findOrCreate({
+        //     where: {
+        //         firstName: signupData.given_name,
+        //         email: signupData.email
+        //     },
+        //     defaults: finalSignUpDetails,
+        //     raw: true
+        // })
+        //     .then(([exist, create]) => {
+        //         console.log("manual", exist);
+        //         console.log("mau", create);
 
-                let data = exist;
-                if (create) {
-                    data = exist.dataValues;
-                }
+        //         let data = exist;
+        //         if (create) {
+        //             data = exist.dataValues;
+        //         }
 
-                jwt.sign(
-                    { allData: data },
-                    config.SECRET,
-                    { expiresIn: "1d" },
-                    (err, token) => {
-                        if (!err) {
-                            data.token = token;
-                            // data.imgUrl = req.body.imgUrl;
-                            // console.log('this is working!', data);
-                            res.send(data);
-                            // res.send(data);
-                        } else {
-                            console.log(err);
-                        }
+        //         jwt.sign(
+        //             { allData: data },
+        //             config.SECRET,
+        //             { expiresIn: "1d" },
+        //             (err, token) => {
+        //                 if (!err) {
+        //                     data.token = token;
+        //                     // data.imgUrl = req.body.imgUrl;
+        //                     // console.log('this is working!', data);
+        //                     res.send(data);
+        //                     // res.send(data);
+        //                 } else {
+        //                     console.log(err);
+        //                 }
+        //             }
+        //         );
+        //     })
+        //     .catch(err => console.log(err));
+        // console.log(req.body.id_token);
+
+        let IdToken = req.body.tokenObj.id_token;
+        // console.log(req.body.imgUrl);
+        var clientId = config.GOOGLE_CLIENT_ID;
+        verifier.verify(IdToken, clientId, function(err, tokenInfo) {
+            if (!err) {
+                // use tokenInfo in here.
+                //   console.log(tokenInfo.given_name, tokenInfo.family_name, tokenInfo.email);
+                let finalSignUpDetails = {
+                    firstName: tokenInfo.given_name,
+                    lastName: tokenInfo.family_name,
+                    email: tokenInfo.email,
+                    password: undefined
+                };
+
+                console.log(finalSignUpDetails);
+                Auth.findOrCreate({
+                    where: {
+                        firstName: tokenInfo.given_name,
+                        email: tokenInfo.email
+                    },
+                    defaults: finalSignUpDetails,
+                    raw: true
+                })
+                .then(([exist, create]) => {
+                    console.log("manual", exist);
+                    console.log("mau", create);
+
+                    let data = exist;
+                    if (create) {
+                        data = exist.dataValues;
                     }
-                );
-            })
-            .catch(err => console.log(err));
+
+                    jwt.sign(
+                        { allData: data },
+                        config.SECRET,
+                        { expiresIn: "1d" },
+                        (err, token) => {
+                            if (!err) {
+                                data.token = token;
+                                data.imgUrl = req.body.imgUrl;
+                                // console.log('this is working!', data);
+                                res.send(data);
+                                // res.send(data);
+                            } else {
+                                console.log(err);
+                            }
+                        }
+                    );
+                })
+                .catch(err => console.log('yahi hai yrr', err));
+            } else {
+                console.log("error verifying token", err);
+            }
+        });
     });
 };
